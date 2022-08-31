c0f4550e3b4da73f9cfff3e2eb03f81705ad8e06
Juan Bautista Colucci <juanbautistacolucci@gmail.com>
2022-08-30 23:29:05 -0300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC74pBiXuf0-bv2gx66HS3oTMT0NJZQ5Fg",
   authDomain: "kikiwai-react-app.firebaseapp.com",
@@ -18,4 +14,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Esta variable representa la base de datos
 export const db = getFirestore(app)
-
